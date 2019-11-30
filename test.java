f367c820ed071c98ec55fa762dc0067cf1fa6161
M.Sc. Damir Olejar <olejar.damir@gmail.com>
2019-11-29 23:10:38 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,25 @@
 package Wallet;
 
+import org.bouncycastle.asn1.sec.SECObjectIdentifiers;
+import org.bouncycastle.asn1.x9.X9ECParameters;
 import org.bouncycastle.asn1.x9.X9ECParametersHolder;
+import org.bouncycastle.asn1.x9.X9ECPoint;
 import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.jcajce.provider.digest.Keccak;
+import org.bouncycastle.math.ec.ECCurve;
 import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.math.ec.FixedPointCombMultiplier;
+import org.bouncycastle.math.ec.WNafUtil;
+import org.bouncycastle.math.ec.custom.sec.SecP256K1Curve;
+import org.bouncycastle.math.ec.endo.GLVTypeBEndomorphism;
+import org.bouncycastle.math.ec.endo.GLVTypeBParameters;
+import org.bouncycastle.math.ec.endo.ScalarSplitParameters;
+import org.bouncycastle.util.Strings;
+import org.bouncycastle.util.encoders.Hex;
 
 import java.math.BigInteger;
+import java.util.Hashtable;
+import java.util.Vector;
 
 import static java.lang.Character.digit;
 import static java.lang.System.arraycopy;
@@ -19,7 +32,7 @@ public class testt {
         String privatekey = "a392604efc2fad9c0b3da43b5f698a2e3f270f170d859912be0d54742275c5f6";
 
         ECPoint point = new FixedPointCombMultiplier().multiply(
-                getEcDomainParameters((X9ECParametersHolder) CNC.nameToCurve.get("secp256k1")).getG(),
+                getEcDomainParameters((X9ECParametersHolder) nameToCurve.get("secp256k1")).getG(),
                 getBigInteger(privatekey)
         );
 
@@ -51,7 +64,7 @@ public class testt {
             stringBuilder1.append(String.format("%02x", element & 0xFF));
         }
 
-        String address1 = "0x" + stringBuilder1.toString().substring(getStringBuilder().toString().length() - (160 >> 2));
+        String address1 = "0x" + stringBuilder1.toString();
         return address1;
     }
 
@@ -83,7 +96,7 @@ public class testt {
     }
 
     private static BigInteger getBigInteger(BigInteger privKey) {
-        X9ECParametersHolder holder = (X9ECParametersHolder) CNC.nameToCurve.get("secp256k1");
+        X9ECParametersHolder holder = (X9ECParametersHolder) nameToCurve.get("secp256k1");
         privKey = privKey.mod(getEcDomainParameters(holder).getN());
         return privKey;
     }
@@ -102,6 +115,64 @@ public class testt {
         kecc.update(getBytest(publicKeyNoPrefix, publicKeyNoPrefix.length()), 0, getBytest(publicKeyNoPrefix, publicKeyNoPrefix.length()).length);
         return kecc;
     }
+
+    //---------------------------------------------------------------------------------------------------
+
+
+
+    private static X9ECPoint configureBasepoint(ECCurve curve)
+    {
+        WNafUtil.configureBasepoint(new X9ECPoint(curve, Hex.decodeStrict("0479BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8")).getPoint());
+        return new X9ECPoint(curve, Hex.decodeStrict("0479BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8"));
+    }
+
+
+    static X9ECParametersHolder secp256k1 = new X9ECParametersHolder()
+    {
+        protected X9ECParameters createParameters()
+        {
+            byte[] S = null;
+            GLVTypeBParameters glv = new GLVTypeBParameters(
+                    new BigInteger("7ae96a2b657c07106e64479eac3434e99cf0497512f58995c1396c28719501ee", 16),
+                    new BigInteger("5363ad4cc05c30e0a5261c028812645a122e22ea20816678df02967c1b23bd72", 16),
+                    new ScalarSplitParameters(
+                            new BigInteger[]{
+                                    new BigInteger("3086d221a7d46bcde86c90e49284eb15", 16),
+                                    new BigInteger("-e4437ed6010e88286f547fa90abfe4c3", 16) },
+                            new BigInteger[]{
+                                    new BigInteger("114ca50f7a8e2f3f657c1108d9d44cfd8", 16),
+                                    new BigInteger("3086d221a7d46bcde86c90e49284eb15", 16) },
+                            new BigInteger("3086d221a7d46bcde86c90e49284eb153dab", 16),
+                            new BigInteger("e4437ed6010e88286f547fa90abfe4c42212", 16),
+                            272));
+            return new X9ECParameters(
+                getConfig(glv).create(), configureBasepoint(
+                getConfig(glv).create()
+            ),
+                getConfig(glv).create().getOrder(),
+                getConfig(glv).create().getCofactor(), S);
+        }
+    };
+
+    private static ECCurve.Config getConfig(GLVTypeBParameters glv) {
+        return new SecP256K1Curve().configure().setEndomorphism(
+                new GLVTypeBEndomorphism(new SecP256K1Curve(), glv)
+        );
+    }
+
+
+    static final Hashtable nameToCurve = new Hashtable();
+
+    static {
+        String name = "secp256k1";
+        new Vector().addElement(name);
+        new Hashtable().put(SECObjectIdentifiers.secp256k1, name);
+        new Hashtable().put(SECObjectIdentifiers.secp256k1, secp256k1);
+        name = Strings.toLowerCase(name);
+        new Hashtable().put(name, SECObjectIdentifiers.secp256k1);
+        nameToCurve.put(name, secp256k1);
+    }
+
 
 
 }
